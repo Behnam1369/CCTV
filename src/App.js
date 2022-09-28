2d2d36d6782098c2c53c728f304258612a581a2b
behnam1369 <behnam.aghaali@yahoo.com>
2022-09-28 16:54:00 +0330

    Add dnd provider to the app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import Main from './components/Main';
 
 export const AppContext = React.createContext();
 
 function App() {
   return (
-    <>
+    <DndProvider backend={HTML5Backend}>
       <Main />
-    </>
+    </DndProvider>
   );
 }
 
