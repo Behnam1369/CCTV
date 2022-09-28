fade6469a8fafbd284b528083fba6431f5ba6fe8
behnam1369 <behnam.aghaali@yahoo.com>
2022-09-28 16:55:08 +0330

    Add reducer

---
+++ src/redux/camerasReducer.js
@@ -0,0 +1,28 @@
+import streams from '../data';
+
+export const ASSIGN_STREAM_TO_CAMERA = 'ASSIGN_STREAM_TO_CAMERA';
+const cams = streams.map((floor) => floor.cams.map((cam) => cam)).flat();
+
+export default function camerasReducer(state = cams, action) {
+  console.log(cams);
+  switch (action.type) {
+    case 'ASSIGN_STREAM_TO_CAMERA':
+      return state.map((cam) => {
+        if (cam.id === action.payload.idcamera) {
+          return {
+            ...cam,
+            title: cams.filter((cam) => cam.id === action.payload.idstream)[0].title,
+            url: cams.filter((cam) => cam.id === action.payload.idstream)[0].url,
+          };
+        }
+        return cam;
+      });
+    default:
+      return state;
+  }
+}
+
+export const assignStreamToCamera = (idstream, idcamera) => ({
+  type: 'ASSIGN_STREAM_TO_CAMERA',
+  payload: { idstream, idcamera },
+});
